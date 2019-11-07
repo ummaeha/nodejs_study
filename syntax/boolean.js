17e6eebe8f18c5b1a46b85b40e28eded40a5c70a
ummaeha <raya000@naver.com>
2019-11-07 13:09:49 +0900

    2019/11/07 배열과 반복문

---
+++ syntax/boolean.js
@@ -0,0 +1,4 @@
+//true와 false로 이루어져 있음
+console.log(true);
+console.log(false);
+//변수이름으로 쓸 수 없음  true = 1;
---
+++ syntax/comparison.js
@@ -0,0 +1,8 @@
+console.log(1+1);
+console.log(1==1); //true
+console.log(1==2); //false
+console.log(1>2); //false
+console.log(1===1); //이유가 없으면 === 쓰기
+
+name = 1; //대입연산자
+name == 1; //비교연산자
