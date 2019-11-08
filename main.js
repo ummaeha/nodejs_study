fbed904dd42628d7a60eb6144cecf71475ed88c1
ummaeha <raya000@naver.com>
2019-11-09 01:15:53 +0900

    19/11/09 섹션2 완료

--- main.js
+++ main.js
@@ -3,68 +3,66 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url'); //url이라는 모듈을 url이라는 변수를 통해 사용할거다
 
+function templateHTML(title, list, body) {
+  return ` 
+  <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    ${body}
+  </body>
+  </html>
+  `;
+}
+function templateLIST(filelist) {
+  //[ 'CSS', 'HTML', 'JavaScript' ]라고 data 디렉토리에 있는 파일들이 담겨있는 리스트인 filelist에서 불러옴
+  var list = '<ul>';
+  var i = 0 ;
+  while(i < filelist.length) {
+    list = list + `<li><a href="/?id=${filelist[i]}"> ${filelist[i]}</a></li>`;
+    i++;
+  }
+  list = list + '</ul>';
+  return list;
+}
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
-   
-    console.log(queryData.id);
-    
     if(pathname === '/'){
       if(queryData.id === undefined) {
         //query스트링에 따라 파일명이 생성됨 - data 디렉토리 내 각 이름(HTML,CSS,JavaScript)
-        fs.readFile(`data/${queryData.id}`,'utf8',function(err,description){
-        var title ='Welcome';
-        var description = 'Hello, Node.js!'
-        //1단제목은 동적으로, 웹페이지 내용은 정적으로 만드는데 성공!
-        var template = ` 
-        <!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          <ol>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ol>
-          <h2>${title}</h2>
-          <p>${description}</p>
-        </body>
-        </html>
-        `;
-        response.writeHead(200);
-        response.end(template);
-      }); } else {
-        fs.readFile(`data/${queryData.id}`,'utf8',function(err,description){
+        //fs.readFile(`data/${queryData.id}`,'utf8',function(err,description){
+        fs.readdir('./data', function(error, filelist){
+          //console.log(filelist); 로그 찍어보면 [ 'CSS', 'HTML', 'JavaScript' ]가 나옴
+          var title ='Welcome';
+          var description = 'Hello, Node.js!'
+          var list = templateLIST(filelist);
           //1단제목은 동적으로, 웹페이지 내용은 정적으로 만드는데 성공!
-          var title = queryData.id;
-          var template = ` 
-          <!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            <ol>
-              <li><a href="/?id=HTML">HTML</a></li>
-              <li><a href="/?id=CSS">CSS</a></li>
-              <li><a href="/?id=JavaScript">JavaScript</a></li>
-            </ol>
-            <h2>${title}</h2>
-            <p>${description}</p>
-          </body>
-          </html>
-          `;
+          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+
           response.writeHead(200);
           response.end(template);
-        });
+          })
+      //}); 
+        } else { //id 값이 있을 때
+          fs.readdir('./data', function(error, filelist){
+            var list = templateLIST(filelist);
+
+            fs.readFile(`data/${queryData.id}`,'utf8',function(err,description){
+              //1단제목은 동적으로, 웹페이지 내용은 정적으로 만드는데 성공!
+              var title = queryData.id;
+              var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);;
+              response.writeHead(200);
+              response.end(template);
+            });
+          });
       } 
     }else {
         response.writeHead(404);
---
+++ syntax/function2.js
@@ -0,0 +1,12 @@
+//함수의 입력
+//round - 반올림
+console.log(Math.round(1.6)); //2
+console.log(Math.round(1.4)); //1
+
+function sum(first, second) {//매개변수 parameter
+    return first + second ;
+}
+
+sum(2,4); //입력값(인자) arguments
+
+console.log(sum(2,4));
